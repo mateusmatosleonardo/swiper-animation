c353b4f22b76aaa533b4fc1a06d6d439fb9cd218
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-26 21:22:16 -0300

    refactor: refactoring card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ImageBackground, Text } from 'react-native';
+import { View, Image, Text } from 'react-native';
 import { ICard } from '../../../App';
 import { styles } from './styles';
 import Reload from '@expo/vector-icons/MaterialCommunityIcons';
@@ -9,23 +9,7 @@ import Star from '@expo/vector-icons/Entypo';
 export function Card({ picture, name, age }: ICard) {
   return (
     <View style={styles.container}>
-      <ImageBackground source={picture} style={{ flex: 1 }} imageStyle={{ borderRadius: 8 }}>
-        <View style={styles.wrapperInfo}>
-          <Text style={styles.textName}>{name}</Text>
-          <Text style={styles.age}>{age}</Text>
-        </View>
-        <View style={styles.boxIcons}>
-          <View style={[styles.wrapperIcon, { borderColor: 'yellow' }]}>
-            <Reload name='reload' size={31} color='yellow' />
-          </View>
-          <View style={[styles.wrapperIcon, { borderColor: 'red', width: 70, height: 70, borderRadius: (70 / 2) }]}>
-            <Close name='close-a' size={31} color='red' />
-          </View>
-          <View style={[styles.wrapperIcon, { borderColor: 'blue', }]}>
-            <Star name='star' size={31} color='blue' />
-          </View>
-        </View>
-      </ImageBackground>
+      <Image source={picture} style={{ width: 250, height: 250 }} />
     </View>
   );
 }
